68cda90435e3f407351563456c5f4db8b6a0410a
triplr <triplr.dev@gmail.com>
2020-12-14 20:25:51 -0800

    created aicp-init.sh

--- AICP/aicp-init.sh
+++ AICP/aicp-init.sh
@@ -1,50 +1,41 @@
 #!/bin/bash
-# AEX			repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 9.x
-# AICP			repo init --depth=1 -u https://github.com/AICP/platform_manifest.git -b p9.0
-# AOKP			repo init --depth=1 -u https://github.com/AOKP/platform_manifest.git -b pie
-# LineageOS		repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
-# Pixel Expierence	repo init --depth=1 -u https://github.com/PixelExperience/manifest -b pie
-# ViperOS		repo init --depth=1 -u https://github.com/ViperOS/viper_manifest.git -b pie
-# xenon 		repo init --depth=1 -u https://github.com/TeamHorizon/platform_manifest.git -b p
 
-export BUILDd=~/android/9/AICP
+manifests='../ROM_MANIFESTS/manifests.sh'
+if 
+	[ -f $manifests ];
+		then
+			echo 'Loading Rom Manifests';
+			cat ../ROM_MANIFESTS/manifests.sh ;
+			. ../ROM_MANIFESTS/manifests.sh ;
+		else
+			echo 'manifests.sh does not exist';
+			
+fi
+			 
+
+export BUILDd=~/android/AICP-11.x
 export INITd=$BUILDd/.repo
 export ROOMd=$BUILDd/.repo/local_manifests
-export manifest='https://github.com/AICP/platform_manifest.git -b p9.0'
 
-# check to see if build dir exists if not create
-if [ ! -d $BUILDd ] ;
-	then
-		mkdir -pv $BUILDd ;
-	else
-		echo '$BUILDd exists'
-fi
-
-
-# check to see if manifest .repo exists, if not create, init, and sync
 if 
    [ ! -d $INITd ];
-	then
-		cd $BUILDd ;
-		repo init --depth=1 -u	$manifest
-		repo sync -c -j16 --force-sync --no-clone-bundle --no-tags	;
-		echo "REPO init and sync complete"		
-     	else
-    		echo '$INITd exists ' 
+   	then
+	mkdir -pv $INITd;
+	cd $BUILDd ;
+	repo init --depth=1 -u $initAEX ;
+	repo sync -c -j32 --force-sync --no-clone-bundle --no-tags	;
+	echo "REPO init and sync complete"		
+         else
+    echo ' repo dir exists ' 
 fi
 
-# check to see if roomservice dir exists if not , create
 if 
    [ ! -d $ROOMd ];
       then
     	mkdir -pv $ROOMd ;
 	cd $BUILDd ;
          else
-    echo '$ROOMd dir exists ' 
+    echo ' roomservice dir exists ' 
 fi
-
-# change to build dir, and prepare to build
-cd $BUILDd
-pwd
 
 
---
+++ ROM_MANIFESTS/manifests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export initAEX="git://github.com/aex-tmp/manifest.git -b 11.x"
+export initAICP="https://github.com/AICP/platform_manifest.git -b r11.1"
+export initAOSIP="git://github.com/AOSiP/platform_manifest.git -b eleven"
+export initAOSP="https://android.googlesource.com/platform/manifest -b android-11.0.0_r25"
+export initLOS="git://github.com/LineageOS/android.git -b lineage-18.1"
+export initOMNI="git://github.com/omnirom/android.git -b android-11"
+export initPixelPlus="https://github.com/PixelExperience/manifest -b eleven"
+
+
